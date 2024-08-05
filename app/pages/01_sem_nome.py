09c10d66397edc95f0796ad9ce69392bf3ca2914
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-05 16:51:06 -0300

    Update 01_sem_nome.py

--- app/pages/01_sem_nome.py
+++ app/pages/01_sem_nome.py
@@ -4,6 +4,11 @@ import streamlit as st
 import os
 
 # Configurar a conexão com o MySQL
+db_user = st.secrets["db_user"]
+db_password = st.secrets["db_password"]
+db_host = st.secrets["db_host"]
+db_port = st.secrets["db_port"]
+db_name = st.secrets["db_name"]
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 query = 'SELECT * FROM data_mall.indicadores'
 df = pd.read_sql(query, engine)
