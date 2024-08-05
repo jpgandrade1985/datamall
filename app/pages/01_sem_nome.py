07674f656a5c2749bbc711fe1e8e11d250eb7479
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-05 16:31:34 -0300

    Update 01_sem_nome.py

--- app/pages/01_sem_nome.py
+++ app/pages/01_sem_nome.py
@@ -2,6 +2,18 @@ import pandas as pd
 from sqlalchemy import create_engine
 import streamlit as st
 
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB USER:", st.secrets["db_user"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_user"] == st.secrets["db_user"],
+
 # Configurar a conexão com o MySQL
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
