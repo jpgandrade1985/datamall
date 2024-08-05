905091ef2c68038122ade60a9fc226917123f2e9
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-05 18:58:49 -0300

    Update get_data.py

--- code/get_data.py
+++ code/get_data.py
@@ -2,11 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # Configurar a conexão com o MySQL
-db_user = 'data_mall'
-db_password = 'Lgt#0701'
-db_host = 'data_mall.mysql.dbaas.com.br'
-db_port = '3306'  # Porta padrão do MySQL
-db_name = 'data_mall'
+
 
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
