43ef8b09345652e43eff53b4d513eca77e065202
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-05 16:11:33 -0300

    Update 01_sem_nome.py

--- app/pages/01_sem_nome.py
+++ app/pages/01_sem_nome.py
@@ -1,1 +1,10 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import streamlit as st
 
+# Configurar a conexão com o MySQL
+engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+# Consultar dados
+query = 'SELECT * FROM data_mall.indicadores'
+df = pd.read_sql(query, engine)
