1f4da36b1f7a27d489eb2ffff6de4a58d834ea4c
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-07 10:27:20 -0300

    Update 01_Indicadores.py

--- app/pages/01_Indicadores.py
+++ app/pages/01_Indicadores.py
@@ -15,7 +15,8 @@ query = 'SELECT * FROM data_mall.indicadores'
 df = pd.read_sql(query, engine)
 
 # Convert 'mes' column to datetime with the correct format and handle errors
-df['mes'] = pd.to_datetime(df['mes'], format='%Y-%m-%d', errors='coerce')
+#df['mes'] = pd.to_datetime(df['mes'], format='%Y-%m-%d', errors='coerce')
+df['mes'] = pd.to_datetime(df['mes'])
 
 # Remove rows with NaN dates
 df = df.dropna(subset=['mes'])
