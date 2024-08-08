f39ce70217d7e93bb023122a2ded2c66e6f3a743
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-08 13:33:31 -0300

    Update 01_Indicadores.py

--- app/pages/01_Indicadores.py
+++ app/pages/01_Indicadores.py
@@ -75,7 +75,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write("vendas totais")
     fig1 = px.line(filtered_df, x="mes", y="venda_total", color='shopping')
-    fig1.update_layout(xaxis={'showticklabels': True, 'titles': ''})
+    fig1.update_layout(xaxis={'showticklabels': True, 'title': ''})
     st.plotly_chart(fig1, use_container_width=True)
 
 with col2:
