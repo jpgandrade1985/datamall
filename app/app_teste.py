7d19881a1e887ca2dc5f47c4336abb9311fe47a5
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 09:40:44 -0300

    Update app_teste.py

--- app/app_teste.py
+++ app/app_teste.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 import plotly.express as px
 
-from teste import *
+from teste import filtered_df
 
 def run():
     st.set_page_config(page_title="Legatus Data Mall", layout="wide")
