73c74b5a186b6e56854986449c9acfe92ade95b7
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 10:21:14 -0300

    Update geral.py

--- app/geral.py
+++ app/geral.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 import plotly.express as px
 
-def geral(filtered_df):
+def graphs(filtered_df):
     #st.set_page_config(page_title="Legatus Data Mall", layout="wide")
     #Subtitle
     st.subheader("Geral")
