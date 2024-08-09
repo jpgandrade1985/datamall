1bd1eaf52055c7f0cfd87a9abb105aed8d42ea43
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 11:51:15 -0300

    Update teste.py

--- app/teste.py
+++ app/teste.py
@@ -28,6 +28,7 @@ df = df.dropna(subset=['mes'])
 df['mes_formatted'] = df['mes'].dt.strftime('%b-%y')
 df['month'] =df['mes'].dt.month
 df['year'] =df['mes'].dt.year
+df['nome_mes'] = df['mes'].dt.strftime('%B')
 
 #sort by date
 df = df.sort_values(by='mes', ascending=True)
