fdbf6c78949681f53e1ef8cfd86d480cea016a72
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 14:04:48 -0300

    Update teste.py

--- app/teste.py
+++ app/teste.py
@@ -4,6 +4,7 @@ import datetime
 import streamlit as st
 import os
 import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Legatus Data Mall", layout="wide")
 
