32366bff434594b9c7b9d4c70917a107a9c91c7c
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 09:54:48 -0300

    Update geral.py

--- app/geral.py
+++ app/geral.py
@@ -4,6 +4,7 @@ import datetime
 import streamlit as st
 import os
 import plotly.express as px
+from teste import filtered_df
 
 def run():
     #st.set_page_config(page_title="Legatus Data Mall", layout="wide")
