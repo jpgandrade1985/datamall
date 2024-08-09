32683314dc106d9722c487182421268feaf548d2
jpgandrade1985 <jpgandrade@gmail.com>
2024-08-09 14:28:32 -0300

    Update vendas.py

--- app/vendas.py
+++ app/vendas.py
@@ -26,18 +26,22 @@ def graphs(filtered_df):
     with col2:
         st.write("vendas/m² total ano x ano")
         fig2 = px.line(filtered_df, color_discrete_sequence=px.colors.qualitative.T10, x="nome_mes", y="venda_total_m2", color='ano', facet_row="shopping")
-        fig2.update_layout(xaxis={'showticklabels': True, 'title': ''}, margin=dict(l=0, r=0, t=0, b=2))
+        fig1.update_traces(hovertemplate=None)
+        fig2.update_layout(xaxis={'showticklabels': True, 'title': ''}, margin=dict(l=0, r=0, t=0, b=2), hovermode="x")
         fig2.update_yaxes(title_text="vendas/m²", row=1)
         fig2.update_yaxes(title_text="vendas/m²", row=2)
+        fig1.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
         st.plotly_chart(fig2, use_container_width=True)
     
     
     with col3:
         st.write("vendas/m² ocupado ano x ano")
         fig3 = px.line(filtered_df, color_discrete_sequence=px.colors.qualitative.T10, x="nome_mes", y="venda_total_m2_ocupado", color='ano', facet_row="shopping")
-        fig3.update_layout(xaxis={'showticklabels': True, 'title': ''}, margin=dict(l=0, r=0, t=0, b=2))
+        fig1.update_traces(hovertemplate=None)
+        fig3.update_layout(xaxis={'showticklabels': True, 'title': ''}, margin=dict(l=0, r=0, t=0, b=2), hovermode="x")
         fig3.update_yaxes(title_text="vendas/m²", row=1)
         fig3.update_yaxes(title_text="vendas/m²", row=2)
+        fig1.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
         st.plotly_chart(fig3, use_container_width=True)
     
     #Subtitle
