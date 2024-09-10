b1cfc1f5398fbb5ca5ede91cad5c627e74cfcae1
jpgandrade1985 <jpgandrade@gmail.com>
2024-09-10 14:27:35 -0300

    Update vendas.py

--- app/vendas.py
+++ app/vendas.py
@@ -15,7 +15,7 @@ def graphs(filtered_df):
     
     with col1:
         st.write("vendas ano x ano")
-        fig1 = px.line(filtered_df, color_discrete_sequence=px.colors.qualitative.T10, x="nome_mes", y="venda_total", color='shopping', facet_row="ano")
+        fig1 = px.line(filtered_df, color_discrete_sequence=px.colors.qualitative.T10, x="nome_mes", y="venda_total", color='ano', facet_row="shopping")
         fig1.update_traces(hovertemplate=None)
         fig1.update_layout(xaxis={'showticklabels': True, 'title': ''}, margin=dict(l=0, r=0, t=0, b=2), hovermode="x")
         fig1.update_yaxes(title_text="vendas", row=1)
